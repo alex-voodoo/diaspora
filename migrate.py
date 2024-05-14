f7e36170326081b84ba2ff0db804da64e6006832
Alexander Dunaev <alexander.dunaev@gmail.com>
2024-05-14 16:23:17 +0200

    Added support for DB migrations Added support for storing the confidence of spam detection

---
+++ migrate.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+import sqlite3
+
+
+def main() -> None:
+    conn = sqlite3.connect("people.db")
+    c = conn.cursor()
+
+    c.execute("CREATE TABLE IF NOT EXISTS \"migrations\" ("
+              "\"name\" TEXT UNIQUE,"
+              "PRIMARY KEY(\"name\")"
+              ")")
+
+    migrations_directory = pathlib.Path(__file__).parent / "migrations"
+    migration_filenames = sorted(filename for filename in os.listdir(migrations_directory) if filename.endswith(".txt"))
+    for migration_filename in migration_filenames:
+        skip = False
+        for _ in c.execute("SELECT name FROM migrations WHERE name=?", (migration_filename,)):
+            print("Migration {filename} is already applied, skipping".format(filename=migration_filename))
+            skip = True
+
+        if skip:
+            continue
+
+        with open(migrations_directory / migration_filename) as inp:
+            sql = inp.read()
+            c.execute(sql)
+
+            c.execute("INSERT INTO migrations(name) VALUES(?)", (migration_filename,))
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
