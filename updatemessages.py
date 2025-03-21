b6597b64b67709426db06a4f11547a3ec1e489c7
Alexander Dunaev <alexander.dunaev@gmail.com>
2025-03-21 10:12:44 +0100

    Fixed the babel command (some typos in parameters)

--- updatemessages.py
+++ updatemessages.py
@@ -3,7 +3,7 @@
 import subprocess
 
 # Should a new translation language be added, register it using the following command:
-# pybabel init --input_file=locales/bot.pot --output_dir=locales --domain=bot --locale=<language code>
+# pybabel init --input-file=locales/bot.pot --output-dir=locales --domain=bot --locale=<language code>
 
 subprocess.run(['pybabel', 'extract', '--input-dirs=.', '--output-file=locales/bot.pot', '--ignore-dirs=venv', '--no-wrap'])
 
