e37119a6513f96586e35a22eb50a63a6b2dc1a7f
Alexander Dunaev <alexander.dunaev@gmail.com>
2025-03-31 23:40:44 +0200

    Increased probability of sending an URL

--- features/aprils_fool.py
+++ features/aprils_fool.py
@@ -14,13 +14,14 @@ import settings
 URLS_FILENAME = "aprils_fool_urls.txt"
 URLS_FILE_PATH = pathlib.Path(__file__).parent / "resources" / URLS_FILENAME
 
-
 COMMAND_ASTANAVITES = "astanavites"
 
 EMOJI_REPLIES = ("✋", "🛑", "🙅‍♀️", "️⛔", "❌", "🚧")
 urls = None
 latest_url_timestamp = None
 MIN_URL_DELAY = 15
+URL_CHANCE = 50
+
 
 async def handle_command_astanavites(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle the April's fool command"""
@@ -34,11 +35,12 @@ async def handle_command_astanavites(update: Update, context: ContextTypes.DEFAU
             for line in f.readlines():
                 urls.append(line.strip())
 
-
     if datetime.datetime.now().month != 4 or datetime.datetime.now().day != 1:
         return
 
-    if random.randint(0, 100) < 10 and (latest_url_timestamp is None or datetime.datetime.now() - datetime.timedelta(minutes=MIN_URL_DELAY) > latest_url_timestamp):
+    if random.randint(0, 100) < URL_CHANCE and (
+            latest_url_timestamp is None or datetime.datetime.now() - datetime.timedelta(
+            minutes=MIN_URL_DELAY) > latest_url_timestamp):
         latest_url_timestamp = datetime.datetime.now()
         await context.bot.send_message(chat_id=settings.MAIN_CHAT_ID, text=random.sample(urls, 1)[0])
     else:
