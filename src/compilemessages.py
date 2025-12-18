88699be56e486c82972e3edb0fbfb9867497201a
Alexander Dunaev <alexander.dunaev@gmail.com>
2025-12-18 19:18:09 +0100

    Updated translation scripts so that they could be run the same way as the bot Updated translations (no new strings)

--- src/compilemessages.py
+++ src/compilemessages.py
@@ -1,5 +1,17 @@
-#!./venv/bin/python
+"""
+Compile the translations so that they could be used by the application
+"""
 
+import os
+import pathlib
 import subprocess
 
-subprocess.run(['pybabel', 'compile', '--directory=locales', '--domain=bot', '--use-fuzzy'])
+
+def main():
+    os.chdir(pathlib.Path(__file__).parent)
+
+    subprocess.run(['pybabel', 'compile', '--directory=locales', '--domain=bot', '--use-fuzzy'])
+
+
+if __name__ == "__main__":
+    main()
--- src/updatemessages.py
+++ src/updatemessages.py
@@ -1,10 +1,24 @@
-#!./venv/bin/python
+"""
+Update translation tables with the latest changes in the source code
 
+Should a new translation language be added, register it with the following command:
+
+    pybabel init --input-file=locales/bot.pot --output-dir=locales --domain=bot --locale=<language code>
+"""
+
+import os
+import pathlib
 import subprocess
 
-# Should a new translation language be added, register it using the following command:
-# pybabel init --input-file=locales/bot.pot --output-dir=locales --domain=bot --locale=<language code>
 
-subprocess.run(['pybabel', 'extract', '--input-dirs=.', '--output-file=locales/bot.pot', '--ignore-dirs=venv', '--no-wrap'])
+def main():
+    os.chdir(pathlib.Path(__file__).parent)
 
-subprocess.run(['pybabel', 'update', '--input-file=locales/bot.pot', '--output-dir=locales', '--domain=bot', '--no-wrap'])
+    subprocess.run(
+        ['pybabel', 'extract', '--input-dirs=.', '--output-file=locales/bot.pot', '--ignore-dirs=venv', '--no-wrap'])
+    subprocess.run(
+        ['pybabel', 'update', '--input-file=locales/bot.pot', '--output-dir=locales', '--domain=bot', '--no-wrap'])
+
+
+if __name__ == "__main__":
+    main()
