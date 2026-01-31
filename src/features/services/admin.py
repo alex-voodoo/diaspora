0c74e194a928fabf62ee7c850b9704e4b0ce4fdb
Alexander Dunaev <alexander.dunaev@gmail.com>
2026-01-31 19:34:40 +0100

    Added placeholders for showing services' stats

--- src/features/services/admin.py
+++ src/features/services/admin.py
@@ -14,13 +14,15 @@ from telegram.ext import Application, CallbackQueryHandler, ContextTypes, Conver
 from common import i18n
 from common.admin import get_main_keyboard, register_buttons, has_attachment
 from common.checks import is_admin
+from common.messaging_helpers import reply
 from . import state
 
-_ADMIN_EXPORT_DB, _ADMIN_IMPORT_DB = "services-admin-export-db", "services-admin-import-db"
+_ADMIN_EXPORT_DB, _ADMIN_IMPORT_DB, _ADMIN_STATS = ("services-admin-export-db", "services-admin-import-db",
+                                                    "services-admin-stats")
 _ADMIN_UPLOADING_DB = 1
 
 
-async def _handle_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None | int:
+async def _handle_query(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> None | int:
     query = update.callback_query
     user = query.from_user
 
@@ -36,21 +38,22 @@ async def _handle_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         await user.send_document(json.dumps(state.export_db(), ensure_ascii=False, indent=2).encode("utf-8"),
                                  filename="services.json", reply_markup=None)
     elif query.data == _ADMIN_IMPORT_DB:
-        await query.message.reply_text(trans.gettext("SERVICES_MESSAGE_DM_ADMIN_REQUEST_DB"))
+        await reply(update, trans.gettext("SERVICES_MESSAGE_DM_ADMIN_REQUEST_DB"))
 
         return _ADMIN_UPLOADING_DB
+    elif query.data == _ADMIN_STATS:
+        await _show_stats(update)
 
 
-async def _handle_received_db(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    message = update.effective_message
+async def _handle_received_db(update: Update, _context: ContextTypes.DEFAULT_TYPE) -> int:
     trans = i18n.trans(update.effective_user)
 
     success, error_message = has_attachment(update, "application/json")
     if not success:
-        await message.reply_text(error_message, reply_markup=get_main_keyboard())
+        await reply(update, error_message, get_main_keyboard())
         return ConversationHandler.END
 
-    db_file = await message.document.get_file()
+    db_file = await update.effective_message.document.get_file()
     data = io.BytesIO()
     await db_file.download_to_memory(data)
     data.seek(0)
@@ -64,28 +67,32 @@ async def _handle_received_db(update: Update, context: ContextTypes.DEFAULT_TYPE
 
         state.import_db(data)
 
-        await message.reply_text(trans.gettext("SERVICES_MESSAGE_DM_ADMIN_DB_IMPORTED"), reply_markup=None)
+        await reply(update, trans.gettext("SERVICES_MESSAGE_DM_ADMIN_DB_IMPORTED"))
     except jsonschema.ValidationError as e:
         logging.error(e)
-        await message.reply_text(trans.gettext("SERVICES_MESSAGE_DM_ADMIN_INVALID_JSON"),
-                                 reply_markup=get_main_keyboard())
+        await reply(update, trans.gettext("SERVICES_MESSAGE_DM_ADMIN_INVALID_JSON"), get_main_keyboard())
     except Exception as e:
         logging.error(e)
-        await message.reply_text(trans.gettext("ADMIN_MESSAGE_DM_INTERNAL_ERROR"), reply_markup=get_main_keyboard())
+        await reply(update, trans.gettext("ADMIN_MESSAGE_DM_INTERNAL_ERROR"), get_main_keyboard())
 
     return ConversationHandler.END
+
+
+async def _show_stats(update: Update) -> None:
+    pass
 
 
 def register_handlers(application: Application, group: int):
     application.add_handler(CallbackQueryHandler(_handle_query, pattern=_ADMIN_EXPORT_DB), group=group)
     application.add_handler(
         ConversationHandler(entry_points=[CallbackQueryHandler(_handle_query, pattern=_ADMIN_IMPORT_DB)],
-                            states={_ADMIN_UPLOADING_DB: [
-                                MessageHandler(filters.ATTACHMENT, _handle_received_db)]}, fallbacks=[]),
-        group=group)
+                            states={_ADMIN_UPLOADING_DB: [MessageHandler(filters.ATTACHMENT, _handle_received_db)]},
+                            fallbacks=[]), group=group)
+    application.add_handler(CallbackQueryHandler(_handle_query, pattern=_ADMIN_STATS), group=group)
 
     trans = i18n.default()
     register_buttons(((InlineKeyboardButton(trans.gettext("SERVICES_ADMIN_BUTTON_EXPORT_DB"),
                                             callback_data=_ADMIN_EXPORT_DB),
                        InlineKeyboardButton(trans.gettext("SERVICES_ADMIN_BUTTON_IMPORT_DB"),
-                                            callback_data=_ADMIN_IMPORT_DB)),))
+                                            callback_data=_ADMIN_IMPORT_DB)), (
+                      InlineKeyboardButton(trans.gettext("SERVICES_ADMIN_BUTTON_STATS"), callback_data=_ADMIN_STATS),)))
